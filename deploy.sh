1ddead233d4eec7e0ccd2d17f8f0059a7bdd8e99
michalsnik <msajnog93@gmail.com>
2018-12-27 15:04:04 +0100

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:netguru/design-process.git vuepress:gh-pages
+git push -f git@github.com:netguru/design-process.git master:gh-pages
 
 cd -
