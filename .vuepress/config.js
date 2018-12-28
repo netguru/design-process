165fd3cc944eb783aaab19a8e6e394e1c10677bd
michalsnik <msajnog93@gmail.com>
2018-12-28 16:56:00 +0100

    Set baseUrl

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
   ],
   
   themeConfig: {
+    baseUrl: 'https://design-process.netguru.co',
     exitUrl: 'https://www.netguru.co/design-process',
     company: {
       name: 'Netguru',
